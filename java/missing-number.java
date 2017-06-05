187a95b59d4d44cc75c49ad38fe14a04596df91b
Enrique Arriaga <enriquearriaga@MacBook-Pro-de-Enrique.local>
2017-06-05 13:40:08 +0200

    move to subfolders

--- java/missing-number.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public int missingNumber(int[] nums) {
-        int n=nums.length;
-        int suma = 0;
-        for(int a:nums) suma+=a;
-        int expected = (n*(n+1))/2;
-        return expected-suma;
-    }
-}
